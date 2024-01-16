8e7b643d3e1ec3b43a6f1faa94d35e5e3b0713e5
JakeJeongAiden <146908415+JakeJeongAiden@users.noreply.github.com>
2024-01-15 18:19:19 -0800

    change font color

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,15 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  //you want to immediately return nextResponse.
   return NextResponse.redirect(new URL("events/all", request.url));
 }
 
 export const config = {
   matcher: ["/events"],
 };
+
+/*
+middleware is used a lot during authentication. must be created at src folder for nextJS to detect the middleware.ts
+
+*/
